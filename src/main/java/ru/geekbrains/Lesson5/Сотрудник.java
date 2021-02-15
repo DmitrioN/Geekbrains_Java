2061df303f3c3d8b0b1e4483cd88eed1903e4878
DmitrioN <inkin.dmitrij.1991@mail.ru>
2021-02-15 19:47:18 +0400

    Lesson5

---
+++ src/main/java/ru/geekbrains/Lesson5/Сотрудник.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.Lesson5;
+
+public class Сотрудник {
+    public static String FIO = "";
+    public static String email = "";
+    public static String numberPhone = "";
+    public static String position= "";
+    public static String salary = "";
+    public static String age = "";
+
+    public static void main(String[] args) {
+
+    }
+}
